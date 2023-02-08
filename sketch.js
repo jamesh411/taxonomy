3c7473662956a2ab5ed2f24cee35c65b8a69e170
luciapineda <36985380+luciapineda@users.noreply.github.com>
2023-02-08 00:47:08 -0800

    Code Skeleton, Initial Scripts
    Co-Authored-By: jamesh411 <28360377+jamesh411@users.noreply.github.com>

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
     createCanvas(400, 400);
+    model = new Model();
+
+    //ADD EVENT LISTENERS
+    var button = document.getElementById("add_button");
+    button.addEventListener("click",function(){
+      model.addItem("added content");
+    });
   }
   
   function draw() {
     background(220);
+    textAlign(CENTER);
+    text("This is the P5.js sketch", width/2, height/2);
   }
